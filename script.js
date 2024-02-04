b51fd624bdb817a1e45e373c5bc1f3e210dcd13d
JerryShum <jerryshum2636@gmail.com>
2024-02-04 00:45:22 -0500

    feat: added class validation to the password confirm

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
 const passwordElement = document.querySelector('#password');
 const confirmPasswordElement = document.querySelector('#confirmpassword');
+const submitButtonElement = document.querySelector('.btn-submit');
+
+
+function validatePassword() {
+    let pass = passwordElement.value;
+    let confirmPass = confirmPasswordElement.value;
+
+    if (pass != confirmPass) {
+        confirmPasswordElement.classList.remove('valid');
+        confirmPasswordElement.classList.add('invalid');
+    }
+
+    if (pass === confirmPass) {
+        confirmPasswordElement.classList.add('valid');
+        confirmPasswordElement.classList.remove('invalid');
+    }
+
+}
+
+confirmPasswordElement.addEventListener("keyup", validatePassword);
 
